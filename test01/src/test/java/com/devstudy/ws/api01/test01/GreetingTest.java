5b3942e0ee30b7b63dda1ce0926d0d7d2f9f0e5e
satoh <satoh@tbrave.com>
2020-04-27 11:44:59 +0900

    test01をローカルへコミット

---
+++ test01/src/test/java/com/devstudy/ws/api01/test01/GreetingTest.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ */
+package com.devstudy.ws.api01.test01;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Administrator
+ *
+ */
+
+@SpringBootTest
+class GreetingTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link com.devstudy.ws.api01.test01.Greeting#Greeting(long, java.lang.String)}.
+	 */
+	@Test
+	void testGreeting() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.devstudy.ws.api01.test01.Greeting#getId()}.
+	 */
+	@Test
+	void testGetId() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.devstudy.ws.api01.test01.Greeting#getContent()}.
+	 */
+	@Test
+	void testGetContent() {
+		fail("Not yet implemented");
+	}
+
+}
