f60ecc8ff60f8423bbaa4e6ffd5cec7f8cfca045
satoh <satoh@tbrave.com>
2020-05-05 13:32:12 +0900

    GreetingController修正。greetingMoreメソッド追加

--- test01/src/main/java/com/devstudy/ws/api01/test01/GreetingController.java
+++ test01/src/main/java/com/devstudy/ws/api01/test01/GreetingController.java
@@ -1,6 +1,7 @@
 package com.devstudy.ws.api01.test01;
 
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.Calendar;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,5 +16,18 @@ public class GreetingController {
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+	
+	@GetMapping("/greetingMore")
+	public Greeting greetingMore(@RequestParam(value = "name", defaultValue = "World") String name) {
+		Calendar calendar = Calendar.getInstance();
+		int hour = calendar.get(Calendar.HOUR_OF_DAY);
+		String msg = "";
+		if(hour < 12) {
+			msg = "Good morning, %s";
+		} else {
+			msg = "Good afternoon, %s";
+		}
+		return new Greeting(counter.incrementAndGet(), String.format(msg, name));
 	}
 }
